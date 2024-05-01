75a4ba86897646c05cd71f5400a827228681251d
Surya Pratap <85509906+singham2000@users.noreply.github.com>
2024-05-01 23:03:17 +0530

    updates

--- src/utils/aws.js
+++ src/utils/aws.js
@@ -13,7 +13,7 @@ const s3Client = new S3Client({
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     },
-    region: 'ap-southeast-2'
+    region: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 exports.uploadImage = (file) => {
